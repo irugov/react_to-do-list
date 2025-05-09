1475382a7f54ac30e9ae5460410747534222d43c
Alim <irugovalim@gmail.com>
2025-05-09 09:27:08 +0300

    add react-router routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import Root from './routes/root.jsx'
+import ErrorPage404 from './error-page-404';
+import About from './routes/about.jsx'
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Root />,
+    errorElement: <ErrorPage404 />,
+	},
+  {
+		path: '/about',
+		element: <About />
+	},
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/routes/about.jsx
@@ -0,0 +1,12 @@
+function About() {
+
+    return (
+        <div>
+            <h1>Hi! It is an About Page</h1>
+            <h2>tralalilla tralalla!</h2>
+        </div>
+    );
+
+}
+
+export default About;
